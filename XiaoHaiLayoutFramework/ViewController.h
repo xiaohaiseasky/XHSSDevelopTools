956c55d3d225382dd3d8f99c8c67bb1e7d864ed5
xiaohai <blue_seaskyvideo@163.com>
2017-10-16 20:11:58 +0800

    Initial Commit

---
+++ XiaoHaiLayoutFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiaoHaiLayoutFramework
+//
+//  Created by Apple on 2017/10/16.
+//  Copyright © 2017年 XiaoHai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
